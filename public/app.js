c13c1e021b4742398a058103d6675b477b516d8c
olivercarson-pubnub <oliver.carson@pubnub.com>
2023-03-15 11:14:31 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -23,7 +23,7 @@ async function createPublisher() {
 		headers: { accept: "application/json", "content-type": "application/json" },
 	};
 
-	let response = await fetch("https://dolbyio-pubnub-gdc2023.netlify.app/auth/dolbyio/stream", options);
+	let response = await fetch("https://dolbyio-pubnub-gdc2023.netlify.app/public/auth/dolbyio/stream", options);
 	let res = await response.json();
 	console.log(res);
 
@@ -47,7 +47,7 @@ async function createPubNub() {
 		headers: { accept: "application/json", "content-type": "application/json" },
 	};
 
-	let response = await fetch("https://dolbyio-pubnub-gdc2023.netlify.app/auth/dolbyio/pubnub", options);
+	let response = await fetch("https://dolbyio-pubnub-gdc2023.netlify.app/public/auth/dolbyio/pubnub", options);
 	let res = await response.json();
 	console.log(res);
 
